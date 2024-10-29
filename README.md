5e040ac656b28b166d9124da3639fb08009a48da
unknown <umayerpro175@gmail.com>
2024-10-29 17:29:25 +0600

    e market hub

---
+++ README.md
@@ -0,0 +1,4 @@
+git commit -m "e market hub"
+git branch -M main
+git remote add origin https://github.com/UmayerCoding1/E-Market-hub-server.git
+git push -u origin main
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -18,6 +20,37 @@ app.get('/user', async (req,res) => {
 
     res.send(user)
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Pass}@cluster0.34gmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log(`EMarket server running PORT:${port}`);
