5db0f92375a1e36a7ad7033a21377cdc53af73fe
unknown <umayerpro175@gmail.com>
2024-11-03 23:15:22 +0600

    Auto-commit

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173/'
+  ]
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
