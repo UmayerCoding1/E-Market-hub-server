194fe6576b47d88c0e63f28d0dea6e66fef0f614
unknown <umayerpro175@gmail.com>
2024-11-03 22:41:52 +0600

    Auto-commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("EMarket Hub server is ready");
