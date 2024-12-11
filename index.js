71c21d33808577b7cfd4657b8b35a93bc4f976f5
umayerCoding1 <umayerpro175@gmail.com>
2024-12-11 15:32:44 +0600

    update

--- index.js
+++ index.js
@@ -13,18 +13,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:5175",
-      "https://emarket-hub.web.app",
-      "https://emarket-hub.firebaseapp.com",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-    // allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  cors()
 );
 app.use(express.json());
 // app.use(helmet({
