282df5431450a9ecc3f64e250a46562810396a95
umayerCoding1 <umayerpro175@gmail.com>
2025-01-20 00:20:09 +0600

    save

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const port = process.env.PORT || 8000;
 app.use(
   cors({
     origin: [
-      "https://emarket-hub.web.app"
+      "https://emarket-hub.web.app",
+      "http://localhost:5173"
     ]
   })
 );
@@ -106,8 +107,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipmfa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const uri = "mongodb://localhost:27017/"
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipmfa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = "mongodb://localhost:27017/"
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
